d7e18bd5d09bd6598c31e04037eb3e496ef3586c
samudiperera09 <samudiperera125@gmail.com>
2024-07-05 00:53:35 +0530

    Admin Business Management Table Done

--- wandr-web/src/app/api/admin/businesses/page.tsx
+++ wandr-web/src/app/api/admin/businesses/page.tsx
@@ -2,19 +2,21 @@
 
 import AdminHeader from '@/components/admin/AdminHeader'
 import AdminSidebar from '@/components/admin/AdminSideBar';
+import BusinessTable from '@/components/admin/BusinessTable';
 
-const AdminBusinesses: React.FC = () => {
+const AdminPlaces: React.FC = () => {
   return (
     <div className="flex h-screen">
       <AdminSidebar active={'Businesses'}/>
       <div className="flex-1 flex flex-col">
-          <AdminHeader page={'Businesses'} />
+        <AdminHeader page={'Businesses'} />
+        
         <div className="flex-1 overflow-y-auto">
-          <h1>Businesses</h1>
+          <BusinessTable />
         </div>
       </div>
     </div>
   )
 }
 
-export default AdminBusinesses;
+export default AdminPlaces;
