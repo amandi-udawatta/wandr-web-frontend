8c027cae85a158f9cd90a8aa5476c542e6a689b1
samudiperera09 <samudiperera125@gmail.com>
2024-07-27 01:09:42 +0530

    Blog Page Carousel Added but need some fixes

--- wandr-web/src/app/api/blogs/page.tsx
+++ wandr-web/src/app/api/blogs/page.tsx
@@ -2,18 +2,24 @@
 import React from 'react';
 import BlogPost from './blogPost/page';
 import { Button } from 'antd'; // Assuming this is where your BlogPost component is located
+import Navbar from '@/components/general/Navbar';
+import Footer from '@/components/general/Footer';
+import {BlogHero} from '@/components/general/BlogHero';
+import BlogStatsCarousel from '@/components/general/BlogStatsCarousel';
 
 const BlogPage = () => {
   return (
     <div>
-      <h1>Blog</h1>
-      <BlogPost
+      <Navbar/>
+      <BlogHero/>
+      {/* <BlogPost
         title="My First Blog Post"
         author="John Doe"
         date="January 1, 2022"
         content="<p>This is my first blog post. Welcome!</p>"
-      />
-      <Button type="primary">Button</Button>
+      /> */}
+      {/* <Button type="primary">Button</Button> */}
+      <Footer/>
     </div>
   );
 };
