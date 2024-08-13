8d999a9835f233c7d2db9e529e6a26868ba3f11b
samudiperera09 <samudiperera125@gmail.com>
2024-08-13 13:26:56 +0530

    Added Images for Places

--- wandr-web/src/components/admin/PlacesTable.tsx
+++ wandr-web/src/components/admin/PlacesTable.tsx
@@ -114,7 +114,7 @@ const PlacesTable = () => {
       key: 'name',
       render: (text: string, record: any) => (
         <div className="flex items-center space-x-2 flex-row">
-          {/* <Avatar src={record.imageUrl} /> */}
+          <Avatar src={record.imageUrl} />
           <span>{text}</span>
         </div>
       ),
