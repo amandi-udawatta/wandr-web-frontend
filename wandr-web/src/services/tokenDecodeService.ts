f6d8fdde5c437e15b68ca985777b5f1b59076c08
samudiperera09 <samudiperera125@gmail.com>
2024-12-03 20:22:58 +0530

    added unauthorized page

--- wandr-web/src/services/tokenDecodeService.ts
+++ wandr-web/src/services/tokenDecodeService.ts
@@ -23,3 +23,37 @@ export const getIdFromToken = (): number | null => {
     return null;
   }
 };
+
+export const getPlanIdFromToken = (): number | null => {
+  const token = Cookies.get('accessToken'); // Adjust the key if necessary
+
+  if (!token) {
+    console.error('No token found in cookies');
+    return null;
+  }
+
+  try {
+    const decoded: { plan: number } = jwtDecode(token);
+    return decoded.plan || null;
+  } catch (error) {
+    console.error('Error decoding token:', error);
+    return null;
+  }
+};
+
+export const getRoleFromToken = (): string | null => {
+  const token = Cookies.get('accessToken'); // Adjust the key if necessary
+
+  if (!token) {
+    console.error('No token found in cookies');
+    return null;
+  }
+
+  try {
+    const decoded: { role: string } = jwtDecode(token);
+    return decoded.role || null;
+  } catch (error) {
+    console.error('Error decoding token:', error);
+    return null;
+  }
+};
